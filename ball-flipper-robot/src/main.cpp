fde4b068e7aad906c24a4b0373acde611fa86602
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2021-09-09 10:17:06 -0400

    code

---
+++ ball-flipper-robot/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       EnZon3, Creator of 024.96 Team                            */
+/*    Created:      Thu Sep 09 2021                                           */
+/*    Description:  Official Code for the 024.96 Team                         */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
